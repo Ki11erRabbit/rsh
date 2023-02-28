23ca34a4cb1a9d2dd2df91ef7e49d7b110fd939a
ki11errabbit <ki11errabbit@posteo.net>
2023-02-28 09:46:07 -0700

    layed out some basics of the shell struct

---
+++ src/shell.rs
@@ -0,0 +1,20 @@
+
+
+
+pub struct Shell {
+    // variables
+    local_vars: HashMap<String, LocalVar>,
+    local_var_stack: Vec<HashMap<String, LocalVar>>,
+    var_table: BTreeMap<String, Var>,
+    // directory
+    curr_directory: String,
+    physical_directory: String,
+    //jobs
+    jobctl: bool,
+    job_warning: i32,
+    background_pid: Pid,
+    vforked: bool,
+    tty_fd: i32,
+    job_table: Vec<Job>,
+    current_job: Option<Job>,
+}
