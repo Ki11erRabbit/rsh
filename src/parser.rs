ce690f7a101a32d92cc710204962b9297ca5f710
ki11errabbit <ki11errabbit@posteo.net>
2023-03-01 21:09:43 -0700

    fixed bugs in grammar and got a test working

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,11 @@ use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
-#[grammar = "script_grammar.pest"] // relative to project `src`
+#[grammar = "shell_grammar.pest"] // relative to project `src`
 struct ShellParser;
+
+
+
 
 
 
@@ -22,3 +25,23 @@ pub enum RedirectType {
 
 }
 */
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parser::ShellParser;
+
+    #[test]
+    fn test_pipeline() {
+        let input = "ls -l | grep foo";
+        let pairs = ShellParser::parse(Rule::pipeline, input).unwrap_or_else(|e| panic!("{}", e));
+
+        for pair in pairs {
+            println!("Rule: {:?}", pair.as_rule());
+            println!("Span: {:?}", pair.as_span());
+            println!("Text: {:?}", pair.as_span().as_str());
+        }
+
+    }
+}
